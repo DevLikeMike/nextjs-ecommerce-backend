dc6fc4d24e25bbe3493074d9adba0b83a18261bd
DevLikeMike <mhall.project@gmail.com>
2022-02-12 08:15:23 -0500

    created tshirt endpoint, and attached it to the front end via api on index page

---
+++ src/api/tshirt/controllers/tshirt.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  tshirt controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::tshirt.tshirt');
---
+++ src/api/tshirt/services/tshirt.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * tshirt service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::tshirt.tshirt');
